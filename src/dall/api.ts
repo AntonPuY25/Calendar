0a62cb8ea43a0f3c4d30496af3194545afa58f61
AntonPuY25 <puy25@bk.ru>
2021-05-30 18:12:20 +0300

    refactorApi

--- src/dall/api.ts
+++ src/dall/api.ts
@@ -4,7 +4,7 @@ import axios from "axios"
 export const Api = {
 
     sendEmail(email: string,time:number,cost:string,name:string){
-       return  axios.post('https://smpt-server.herokuapp.com/sendMail',{
+       return  axios.post('https://calendar-smtp.herokuapp.com/sendMail',{
 
             email,time,cost,name
         }).then(response =>{
